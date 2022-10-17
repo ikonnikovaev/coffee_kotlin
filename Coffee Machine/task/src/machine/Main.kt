8f562c5898c970ae93318ca3c86b90be3d912976
elena <ikonnikovaev@gmail.com>
2022-10-17 22:03:27 +0300

    Stage 5: multiple actions until exit

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -21,41 +21,103 @@ var milk = 540
 var beans = 120
 var cups = 9
 
-fun chooseAction() {
-    print("Write action (buy, fill, take):")
+fun chooseAction(): Boolean {
+    print("Write action (buy, fill, take, remaining, exit):")
     val action = readln()
     when (action) {
         "buy" -> buy()
         "fill" -> fill()
         "take" -> take()
+        "remaining" -> printSupply()
     }
+    return action != "exit"
+}
+
+fun make_espresso(): Boolean {
+    if (water < ESPRESSO_WATER_PER_CUP) {
+        println("Sorry, not enough water!")
+        return false
+    }
+    if (milk < ESPRESSO_MILK_PER_CUP) {
+        println("Sorry, not enough milk!")
+        return false
+    }
+    if (beans < ESPRESSO_BEANS_PER_CUP) {
+        println("Sorry, not enough beans!")
+        return false
+    }
+    if (cups < 1) {
+        println("Sorry, not enough cups!")
+        return false
+    }
+    println("I have enough resources, making you a coffee!")
+    water -= ESPRESSO_WATER_PER_CUP
+    milk -= ESPRESSO_MILK_PER_CUP
+    beans -= ESPRESSO_BEANS_PER_CUP
+    cups -= 1
+    money += ESPRESSO_PRICE
+    return true
+}
+
+fun make_latte(): Boolean {
+    if (water < LATTE_WATER_PER_CUP) {
+        println("Sorry, not enough water!")
+        return false
+    }
+    if (milk < LATTE_MILK_PER_CUP) {
+        println("Sorry, not enough milk!")
+        return false
+    }
+    if (beans < LATTE_BEANS_PER_CUP) {
+        println("Sorry, not enough beans!")
+        return false
+    }
+    if (cups < 1) {
+        println("Sorry, not enough cups!")
+        return false
+    }
+    println("I have enough resources, making you a coffee!")
+    water -= LATTE_WATER_PER_CUP
+    milk -= LATTE_MILK_PER_CUP
+    beans -= LATTE_BEANS_PER_CUP
+    cups -= 1
+    money += LATTE_PRICE
+    return true
+}
+
+fun make_cappuccino(): Boolean {
+    if (water < CAPPUCCINO_WATER_PER_CUP) {
+        println("Sorry, not enough water!")
+        return false
+    }
+    if (milk < CAPPUCCINO_MILK_PER_CUP) {
+        println("Sorry, not enough milk!")
+        return false
+    }
+    if (beans < CAPPUCCINO_BEANS_PER_CUP) {
+        println("Sorry, not enough beans!")
+        return false
+    }
+    if (cups < 1) {
+        println("Sorry, not enough cups!")
+        return false
+    }
+    println("I have enough resources, making you a coffee!")
+    water -= CAPPUCCINO_WATER_PER_CUP
+    milk -= CAPPUCCINO_MILK_PER_CUP
+    beans -= CAPPUCCINO_BEANS_PER_CUP
+    cups -= 1
+    money += CAPPUCCINO_PRICE
+    return true
 }
 
 fun buy() {
     println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-    val choice = readln().toInt()
+    val choice = readln()
     when (choice) {
-        1 -> {
-            water -= ESPRESSO_WATER_PER_CUP
-            milk -= ESPRESSO_MILK_PER_CUP
-            beans -= ESPRESSO_BEANS_PER_CUP
-            cups -= 1
-            money += ESPRESSO_PRICE
-        }
-        2 -> {
-            water -= LATTE_WATER_PER_CUP
-            milk -= LATTE_MILK_PER_CUP
-            beans -= LATTE_BEANS_PER_CUP
-            cups -= 1
-            money += LATTE_PRICE
-        }
-        3 -> {
-            water -= CAPPUCCINO_WATER_PER_CUP
-            milk -= CAPPUCCINO_MILK_PER_CUP
-            beans -= CAPPUCCINO_BEANS_PER_CUP
-            cups -= 1
-            money += CAPPUCCINO_PRICE
-        }
+        "1" -> make_espresso()
+        "2" -> make_latte()
+        "3" -> make_cappuccino()
     }
 }
 
@@ -84,18 +146,9 @@ fun take() {
     money = 0
 }
 
-/*
-fun calcPossibleCups(supplyWater: Int, supplyMilk: Int, supplyBeans: Int): Int {
-    val w = supplyWater / WATER_PER_CUP
-    val m = supplyMilk / MILK_PER_CUP
-    val b = supplyBeans / BEANS_PER_CUP
-    return minOf(w, m, b)
-}
- */
+
 
 fun main() {
-    printSupply()
-    chooseAction()
-    printSupply()
+    while (chooseAction()) {}
 
 }
