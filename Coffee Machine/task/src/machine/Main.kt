7d539cc71a7301feb2f79b150f962923c2756f89
elena <ikonnikovaev@gmail.com>
2022-10-17 21:45:47 +0300

    Stage 4: buy coffee, fill supplies, take money

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,41 +1,101 @@
 package machine
 
-const val WATER_PER_CUP = 200
-const val MILK_PER_CUP = 50
-const val BEANS_PER_CUP = 15
+const val ESPRESSO_WATER_PER_CUP = 250
+const val ESPRESSO_MILK_PER_CUP = 0
+const val ESPRESSO_BEANS_PER_CUP = 16
+const val ESPRESSO_PRICE = 4
 
+const val LATTE_WATER_PER_CUP = 350
+const val LATTE_MILK_PER_CUP = 75
+const val LATTE_BEANS_PER_CUP = 20
+const val LATTE_PRICE = 7
+
+const val CAPPUCCINO_WATER_PER_CUP = 200
+const val CAPPUCCINO_MILK_PER_CUP = 100
+const val CAPPUCCINO_BEANS_PER_CUP = 12
+const val CAPPUCCINO_PRICE = 6
+
+var money = 550
+var water = 400
+var milk = 540
+var beans = 120
+var cups = 9
+
+fun chooseAction() {
+    print("Write action (buy, fill, take):")
+    val action = readln()
+    when (action) {
+        "buy" -> buy()
+        "fill" -> fill()
+        "take" -> take()
+    }
+}
+
+fun buy() {
+    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    val choice = readln().toInt()
+    when (choice) {
+        1 -> {
+            water -= ESPRESSO_WATER_PER_CUP
+            milk -= ESPRESSO_MILK_PER_CUP
+            beans -= ESPRESSO_BEANS_PER_CUP
+            cups -= 1
+            money += ESPRESSO_PRICE
+        }
+        2 -> {
+            water -= LATTE_WATER_PER_CUP
+            milk -= LATTE_MILK_PER_CUP
+            beans -= LATTE_BEANS_PER_CUP
+            cups -= 1
+            money += LATTE_PRICE
+        }
+        3 -> {
+            water -= CAPPUCCINO_WATER_PER_CUP
+            milk -= CAPPUCCINO_MILK_PER_CUP
+            beans -= CAPPUCCINO_BEANS_PER_CUP
+            cups -= 1
+            money += CAPPUCCINO_PRICE
+        }
+    }
+}
+
+fun fill() {
+    println("Write how many ml of water you want to add:")
+    water += readln().toInt()
+    println("Write how many ml of nilk you want to add:")
+    milk += readln().toInt()
+    println("Write how many grams of coffee beans you want to add:")
+    beans += readln().toInt()
+    println("Write how many disposable cups you want to add:")
+    cups += readln().toInt()
+}
+
+fun printSupply() {
+    println("The coffee machine has:\n" +
+            "$water ml of water\n" +
+            "$milk ml of milk\n" +
+            "$beans of coffee beans\n" +
+            "$cups disposable cups\n" +
+            "\$$money of money")
+}
+
+fun take() {
+    println("I gave you \$$money")
+    money = 0
+}
+
+/*
 fun calcPossibleCups(supplyWater: Int, supplyMilk: Int, supplyBeans: Int): Int {
     val w = supplyWater / WATER_PER_CUP
     val m = supplyMilk / MILK_PER_CUP
     val b = supplyBeans / BEANS_PER_CUP
     return minOf(w, m, b)
 }
+ */
 
 fun main() {
-    println("Write how many ml of water the coffee machine has:")
-    val supplyWater = readln().toInt()
-    println("Write how many ml of milk the coffee machine has:")
-    val supplyMilk = readln().toInt()
-    println("Write how many grams of coffee beans the coffee machine has:")
-    val supplyBeans = readln().toInt()
-    val possibleCups = calcPossibleCups(supplyWater, supplyMilk, supplyBeans)
-    println("Write how many cups of coffee you will need:")
-    val requiredCups = readln().toInt()
-    if (possibleCups >= requiredCups) {
-        print("Yes, I can make that amount of coffee")
-        if (possibleCups > requiredCups) {
-            println(" (and even ${possibleCups - requiredCups} more than that)")
-        } else {
-            println()
-        }
-    } else {
-        println("No, I can make only $possibleCups cups of coffee")
-    }
+    printSupply()
+    chooseAction()
+    printSupply()
 
-    /*
-    println("For $nCups cups of coffee you will need:\n" +
-            "${nCups * WATER_PER_CUP} ml of water\n" +
-            "${nCups * MILK_PER_CUP} ml of milk\n" +
-            "${nCups * BEANS_PER_CUP} g of coffee beans")
-    */
 }
